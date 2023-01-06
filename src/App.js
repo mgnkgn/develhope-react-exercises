5ab206db3b6714589407ca9e455acce8798b908a
Gunes Akgun <mgunesak@gmail.com>
2023-01-06 18:36:18 +0300

    props1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Hello from "./Hello";
+import Welcome from "./Welcome";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Hello />
+        <Welcome name="Bob" />
       </div>
     );
   }
