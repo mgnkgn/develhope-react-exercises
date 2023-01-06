505bb4bf3757f9a20156d7b313e719f034e23b32
Gunes Akgun <mgunesak@gmail.com>
2023-01-06 22:28:05 +0300

    conitional-rendering-04

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,16 +5,18 @@ export default class Welcome extends Component {
   render() {
     return (
       <>
-        <p>Welcome, {this.props.name}</p>
-        {this.props.age > 18 && this.props.age < 65 && (
-          <Age age={this.props.age} />
-        )}
+        <p>
+          Welcome, <strong>{this.props.name}</strong>
+        </p>
+        {this.props.age > 18 &&
+          this.props.age < 65 &&
+          this.props.name === "John" && <Age age={this.props.age} />}
       </>
     );
   }
 }
 
 Welcome.defaultProps = {
-  name: "Jack",
+  name: "John",
   age: 26,
 };
