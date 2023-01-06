d6f9fe2d3e523ac3408aa15bfe55a02f16bde986
Gunes Akgun <mgunesak@gmail.com>
2023-01-06 16:56:28 +0300

    ex2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Name from "./Name";
 
 const App = () => {
   const hello = <h1>Hello, World!</h1>;
-  return { hello };
+  return (
+    <>
+      <Name name="Bob" />
+    </>
+  );
 };
 
 export default App;
---
+++ src/Name.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Name = (props) => {
+  return <h1>Hello, {props.name}</h1>;
+};
+
+export default Name;
