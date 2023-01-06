d10f744363660483d8e5ef6b01c0befa344cb862
Gunes Akgun <mgunesak@gmail.com>
2023-01-06 17:00:13 +0300

    ex3 => jsx 05

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import Name from "./Name";
 
 const App = () => {
   const hello = <h1>Hello, World!</h1>;
+  const calculateSum = (a, b) => {
+    return <h2>{a + b}</h2>;
+  };
   return (
     <>
       <Name name="Bob" />
+      {calculateSum(2, 4)}
     </>
   );
 };
