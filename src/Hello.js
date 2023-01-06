02e4bc505e5307e4c963de1211ad2276791e783a
Gunes Akgun <mgunesak@gmail.com>
2023-01-06 17:24:28 +0300

    comp07

--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 
+import Message from "./Message";
+
 export default class Hello extends Component {
   render() {
     return (
       <>
         <h1>Hello, World!</h1>
-        <p>What a beautiful day!</p>
+        <Message />
       </>
     );
   }
