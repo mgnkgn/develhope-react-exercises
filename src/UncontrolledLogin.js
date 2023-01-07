e33db906a996bb42168d2735697d6c98135c3318
Gunes Akgun <mgunesak@gmail.com>
2023-01-07 19:04:22 +0300

    forms-06

--- src/UncontrolledLogin.js
+++ src/UncontrolledLogin.js
@@ -10,6 +10,12 @@ export default class UncontrolledLogin extends Component {
       remember: false,
     };
   }
+  _formRef = React.createRef();
+  componentDidMount = () => {
+    console.log(this._formRef.current.elements);
+
+    this._formRef.current.elements.username.focus();
+  };
   submitHandler = (event) => {
     event.preventDefault();
 
@@ -24,10 +30,23 @@ export default class UncontrolledLogin extends Component {
       };
     });
   };
+  resetHandler = (event) => {
+    event.preventDefault();
+    this.setState((state) => {
+      return {
+        name: "",
+        password: "",
+        remember: false,
+      };
+    });
+    this._formRef.current.elements.username.value = "";
+    this._formRef.current.elements.password.value = "";
+    this._formRef.current.elements.remember.checked = false;
+  };
   render() {
     return (
       <div>
-        <form onSubmit={this.submitHandler}>
+        <form onSubmit={this.submitHandler} ref={this._formRef}>
           <input type="text" name="username" defaultValue={this.state.name} />
           <input
             type="text"
@@ -40,7 +59,7 @@ export default class UncontrolledLogin extends Component {
             defaultValue={this.state.remember}
           />
           <button type="submit">Login</button>
-          <button>Reset</button>
+          <button onClick={this.resetHandler.bind(this)}>Reset</button>
         </form>
         <div>Name: {this.state.name}</div>
         <div>Password: {this.state.password}</div>
