3e0af65d48b0a99af70097f70f4601349d8dcccf
Gunes Akgun <mgunesak@gmail.com>
2023-01-08 16:47:06 +0300

    lists-03-form added

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,48 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 
 export default class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: ["Todo1", "Todo2", "Todo3", "Todo4", "Todo5"],
+    };
+  }
+  _formRef = React.createRef();
+  submitHandler = (event) => {
+    event.preventDefault();
+    console.log(this._formRef.current.elements.todo.value);
+    const newTodo = this._formRef.current.elements.todo.value;
+    if (this.state.items.includes(newTodo)) {
+      alert("You already added this todo 🐸");
+      return;
+    }
+    this.setState((state) => {
+      return {
+        items: [...this.state.items, newTodo],
+      };
+    });
+    this._formRef.current.elements.todo.value = "";
+  };
   render() {
-    const items = ["Todo1", "Todo2", "Todo3", "Todo4", "Todo5"];
     return (
-      <ul>
-        {items.map((item) => (
-          <li key={items.length - items.indexOf(item)}>{item}</li>
-        ))}
-      </ul>
+      <>
+        <form
+          onSubmit={this.submitHandler.bind(this)}
+          ref={this._formRef}
+          autoComplete="off"
+        >
+          <input type="text" name="todo" />
+          <button type="submit">Enter Todo</button>
+        </form>
+        <ul>
+          {this.state.items.map((item) => (
+            <li key={this.state.items.length - this.state.items.indexOf(item)}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      </>
     );
   }
 }
