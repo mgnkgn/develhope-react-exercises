ab7235851971bbb308d43445f692d1b0ee537616
Gunes Akgun <mgunesak@gmail.com>
2023-01-24 16:09:45 +0300

    custom hooks 04

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -3,7 +3,9 @@ import { useState } from "react";
 import UseGithub from "./UseGithub";
 
 const GithubUser = (props) => {
-  const { username, imgUrl, link } = UseGithub(props.name);
+  const { username, imgUrl, link, loading, error, fetchData } = UseGithub(
+    props.name
+  );
   // const [username, setUsername] = useState();
   // const [imgUrl, setImgUrl] = useState();
   // const [link, setLink] = useState();
@@ -33,24 +35,30 @@ const GithubUser = (props) => {
         padding: "15px 3.5px",
       }}
     >
-      <div>
-        Username for {props.name}: {username}{" "}
-      </div>
-      <img
-        src={`${imgUrl}`}
-        alt="user-avatar"
-        style={{
-          width: "120px",
-          height: "120px",
-          borderRadius: "50%",
-        }}
-      />{" "}
-      <div style={{ border: "1px dashed royalblue", padding: "7.5px" }}>
-        Go to the profile:{" "}
-        <a href={`${link}`} target="_blank">
-          {link}
-        </a>
-      </div>
+      {!loading && !error && (
+        <>
+          <div>
+            Username for {props.name}: {username}{" "}
+          </div>
+          <img
+            src={`${imgUrl}`}
+            alt="user-avatar"
+            style={{
+              width: "120px",
+              height: "120px",
+              borderRadius: "50%",
+            }}
+          />{" "}
+          <div style={{ border: "1px dashed royalblue", padding: "7.5px" }}>
+            Go to the profile:{" "}
+            <a href={`${link}`} target="_blank">
+              {link}
+            </a>
+          </div>
+        </>
+      )}
+      {!loading && error && <>{error}</>}
+      {loading && <div>Loading...</div>}
     </div>
   );
 };
--- src/UseGithub.js
+++ src/UseGithub.js
@@ -3,22 +3,31 @@ import React, { useState, useEffect } from "react";
 const UseGithub = (receivedName) => {
   const [username, setUsername] = useState();
   const [imgUrl, setImgUrl] = useState();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [link, setLink] = useState();
 
   const fetchData = async (receivedName) => {
-    const data = await fetch(`https://api.github.com/users/${receivedName}`);
+    try {
+      setLoading(true);
+      const data = await fetch(`https://api.github.com/users/${receivedName}`);
 
-    const resolvedData = await data.json();
-    console.log(resolvedData);
-    setUsername(resolvedData.name);
-    setImgUrl(resolvedData.avatar_url);
-    setLink(resolvedData.html_url);
+      const resolvedData = await data.json();
+      console.log(resolvedData);
+      setUsername(resolvedData.name);
+      setImgUrl(resolvedData.avatar_url);
+      setLink(resolvedData.html_url);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     fetchData(receivedName);
   }, [username]);
-  return { username, imgUrl, link };
+  return { username, imgUrl, link, loading, error, fetchData };
 };
 
 export default UseGithub;
